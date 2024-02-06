a514a7135128e5857d858d0c07df539637d751da
leo <silva.leonardoferreira@gmail.com>
2024-02-06 19:23:53 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Silvana e Henrique EU AMO VOCÊS COM TODO O MEU CORAÇÃO s2 s2</h1>
+function Home() {
+  return <h1>Silvana e Henrique EU AMO VOCÊS COM TODO O MEU CORAÇÃO s2 s2</h1>;
 }
 
 export default Home;
