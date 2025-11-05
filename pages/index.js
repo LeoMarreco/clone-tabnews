2a9570d9b307979b9a3aea3dffcad74c74d9e626
leonardo <silva.leonardoferreira@gmail.com>
2025-11-05 07:49:10 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Silvana e Henrique Eu amo vocês com todo meu coração.</h1>
+    return <h1>Silvana e Henrique Eu amo vocês com todo meu coração. S2</h1>
 }
 
 export default Home;
